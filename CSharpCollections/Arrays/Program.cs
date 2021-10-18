3a06dcc204e9d5bc41ca3dcea913552bf0b59ee4
Danilo Silva <danilo.silva@msn.com>
2021-10-18 14:43:25 -0300

    Adicionando Comentários Úteis

--- CSharpCollections/Arrays/Program.cs
+++ CSharpCollections/Arrays/Program.cs
@@ -6,70 +6,93 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            // Elementos que entrarão no Array
             string aulaIntroducao = "Introdução às Coleções";
             string aulaModelando = "Modelando a Classe Aula";
             string aulaConjuntos = "Trabalhando com Conjuntos";
 
-            string[] arrayAulasPrimeiraForma = new string[]
+            // Inicializando um Array populado
+            string[] aulas = new string[]
             {
                 aulaIntroducao,
                 aulaModelando,
                 aulaConjuntos
             };
 
-            string[] arrayAulasSegundaForma = new string[3];
-            arrayAulasSegundaForma[0] = aulaIntroducao;
-            arrayAulasSegundaForma[1] = aulaModelando;
-            arrayAulasSegundaForma[2] = aulaConjuntos;
+            // Declarando um Array vazio de tamanho 3
+            string[] arrayAulasOutraForma = new string[3];
+            // Preenchendo posições no Array pelo índice
+            arrayAulasOutraForma[0] = aulaIntroducao;
+            arrayAulasOutraForma[1] = aulaModelando;
+            arrayAulasOutraForma[2] = aulaConjuntos;
 
-            ImprimirPrimeiraForma(arrayAulasSegundaForma);
-            ImprimirSegundaForma(arrayAulasSegundaForma);
-
-            Console.WriteLine("Primeiro Elemento: " + arrayAulasSegundaForma[0]);
-            Console.WriteLine("Segundo Elemento: " + arrayAulasSegundaForma[arrayAulasSegundaForma.Length - 1]);
+            // A linha abaixo não imprime os elementos!
+            Console.WriteLine(aulas);
             Console.WriteLine();
 
+            Imprimir(aulas);
+
+            // Pegando o primeiro elemento
+            Console.WriteLine("Primeiro Elemento: " + aulas[0]);
+            // Pegando o último elemento
+            Console.WriteLine("Segundo Elemento: " + aulas[aulas.Length - 1]);
+            Console.WriteLine();
+
+            // Modificando um elemento do Array: ASSIM NÃO FUNCIONA!
             aulaIntroducao = "Trabalhando com Arrays";
-            ImprimirPrimeiraForma(arrayAulasSegundaForma);
+            Imprimir(aulas);
 
-            arrayAulasSegundaForma[0] = "Trabalhando com Arrays";
-            ImprimirPrimeiraForma(arrayAulasSegundaForma);
+            // Modificando um elemento do Array: DESSE JEITO FUNCIONA!
+            aulas[0] = "Trabalhando com Arrays";
+            Imprimir(aulas);
 
-            Console.WriteLine("A aula modelando está no índice " + Array.IndexOf(arrayAulasSegundaForma, aulaModelando));
-            Console.WriteLine("A aula modelando está no índice " + Array.LastIndexOf(arrayAulasSegundaForma, aulaModelando));
+            // Localizando a primeira ocorrência no Array
+            Console.WriteLine("A aula modelando está no índice " + Array.IndexOf(aulas, aulaModelando));
+            // Localizando a última ocorrência no Array
+            Console.WriteLine("A aula modelando está no índice " + Array.LastIndexOf(aulas, aulaModelando));
             Console.WriteLine();
 
-            Array.Reverse(arrayAulasSegundaForma);
-            ImprimirPrimeiraForma(arrayAulasSegundaForma);
+            // Revertendo a sequência do Array
+            Array.Reverse(aulas);
+            Imprimir(aulas);
 
-            Array.Reverse(arrayAulasSegundaForma);
-            ImprimirPrimeiraForma(arrayAulasSegundaForma);
+            // Revertendo NOVAMENTE a sequência do Array
+            Array.Reverse(aulas);
+            Imprimir(aulas);
 
-            Array.Resize(ref arrayAulasSegundaForma, 2);
-            ImprimirPrimeiraForma(arrayAulasSegundaForma);
-            
-            Array.Resize(ref arrayAulasSegundaForma, 3);
-            ImprimirPrimeiraForma(arrayAulasSegundaForma);
+            // Redimensionando um Array (truncando a última posição)
+            Array.Resize(ref aulas, 2);
+            Imprimir(aulas);
 
-            arrayAulasSegundaForma[arrayAulasSegundaForma.Length - 1] = "Conclusão";
-            ImprimirPrimeiraForma(arrayAulasSegundaForma);
+            // Redimensionando um Array (deixando a última posição vazia)
+            Array.Resize(ref aulas, 3);
+            Imprimir(aulas);
 
-            Array.Sort(arrayAulasSegundaForma);
-            ImprimirPrimeiraForma(arrayAulasSegundaForma);
+            // Preenchendo a última posição do Array
+            aulas[aulas.Length - 1] = "Conclusão";
+            Imprimir(aulas);
 
+            // Ordenando o Array pela ordem natural dos elementos (alfabética)
+            Array.Sort(aulas);
+            Imprimir(aulas);
+
+            // Copiando um Array em outro
             string[] copia = new string[2];
-            Array.Copy(arrayAulasSegundaForma, 1, copia, 0, 2);
-            ImprimirPrimeiraForma(copia);
+            Array.Copy(aulas, 1, copia, 0, 2);
+            Imprimir(copia);
 
-            string[] clone = arrayAulasSegundaForma.Clone() as string[];
-            ImprimirPrimeiraForma(clone);
+            // Clonando um Array em um novo Array
+            string[] clone = aulas.Clone() as string[];
+            Imprimir(clone);
 
+            // Limpando alguns índices do Array
             Array.Clear(clone, 1, 2);
-            ImprimirPrimeiraForma(clone);
+            Imprimir(clone);
         }
 
-        private static void ImprimirPrimeiraForma(string[] aulas)
+        private static void Imprimir(string[] aulas)
         {
+            // Enumerando um Array (laço FOREACH é mais simples)
             foreach (var aula in aulas)
             {
                 Console.WriteLine(aula);
@@ -78,8 +101,9 @@ namespace Arrays
             Console.WriteLine();
         }
 
-        private static void ImprimirSegundaForma(string[] aulas)
+        private static void ImprimirOutraForma(string[] aulas)
         {
+            // Enumerando um Array (laço FOR permite mais controle)
             for (int i = 0; i < aulas.Length; i++)
             {
                 Console.WriteLine(aulas[i]);
