e7ad1736e3b05f8f7c53a6c0dc45af7a1b5703fa
Danilo Silva <danilo.silva@msn.com>
2021-10-19 09:53:01 -0300

    Aula 06 - Listas Somente Leitura

---
+++ CSharpCollections/ListasSomenteLeitura/Curso.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace ListasSomenteLeitura
+{
+    class Curso
+    {
+        public Curso(string nome, string instrutor)
+        {
+            this.aulas = new List<Aula>();
+            Nome = nome;
+            Instrutor = instrutor;
+        }
+
+        internal void Adiciona(Aula aula)
+        {
+            this.aulas.Add(aula);
+        }
+
+        private IList<Aula> aulas;
+        public IList<Aula> Aulas 
+        { 
+            get { return new ReadOnlyCollection<Aula>(aulas); } 
+        }
+        public string Nome { get; set; }
+        public string Instrutor { get; set; }
+    }
+}
---
+++ CSharpCollections/ListasSomenteLeitura/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListasSomenteLeitura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Curso csharpCollections = new Curso("C# Collections", "Marcelo Oliveira");
+            csharpCollections.Adiciona(new Aula("Trabalhando com Listas", 21));
+            Imprimir(csharpCollections.Aulas);
+        }
+
+        private static void Imprimir(IList<Aula> aulas)
+        {
+            foreach (var aula in aulas)
+            {
+                Console.WriteLine(aula);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
