7f5e312da0e33071ef145048291d1c280e5262da
Danilo Silva <danilo.silva@msn.com>
2021-10-19 11:11:42 -0300

    Aula 07 - Ordenando e Totalizando

--- CSharpCollections/ListasSomenteLeitura/Curso.cs
+++ CSharpCollections/ListasSomenteLeitura/Curso.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace ListasSomenteLeitura
 {
@@ -25,5 +26,25 @@ namespace ListasSomenteLeitura
         }
         public string Nome { get; set; }
         public string Instrutor { get; set; }
+
+        public int TempoTotal { 
+            get 
+            {
+                //int total = 0;
+                //foreach (var aula in aulas)
+                //{
+                //    total += aula.Tempo;
+                //}
+                //return total;
+
+                // LINQ = Language Integrated Query (Consulta Integrada à Linguagem)
+                return aulas.Sum(aula => aula.Tempo);
+            } 
+        }
+
+        public override string ToString()
+        {
+            return $"Curso: {Nome}, Tempo: {TempoTotal}, Aulas:\n\t{string.Join(",\n\t", aulas)}";
+        }
     }
 }
--- CSharpCollections/ListasSomenteLeitura/Program.cs
+++ CSharpCollections/ListasSomenteLeitura/Program.cs
@@ -9,7 +9,32 @@ namespace ListasSomenteLeitura
         {
             Curso csharpCollections = new Curso("C# Collections", "Marcelo Oliveira");
             csharpCollections.Adiciona(new Aula("Trabalhando com Listas", 21));
+
+            // Adicionar 2 aulas
+            csharpCollections.Adiciona(new Aula("Criando uma Aula", 20));
+            csharpCollections.Adiciona(new Aula("Modelando com Coleções", 19));
+            
+            // Imprimir
             Imprimir(csharpCollections.Aulas);
+
+            // Ordenar a lista de aulas
+            //csharpCollections.Aulas.Sort();
+
+            // Copiar a lista para outra lista
+            List<Aula> aulasCopiadas = new List<Aula>(csharpCollections.Aulas);
+
+            // Ordenar a cópia
+            aulasCopiadas.Sort();
+
+            Imprimir(aulasCopiadas);
+
+            // Totalizar o tempo do curso
+            Console.WriteLine(csharpCollections.TempoTotal);
+
+            Console.WriteLine(csharpCollections);
+
+            // Imprimir os detalhes do curso
+
         }
 
         private static void Imprimir(IList<Aula> aulas)
