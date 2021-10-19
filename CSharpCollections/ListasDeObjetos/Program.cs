77809dd87d192f4f01fd0c54c993b02865dd29ed
Danilo Silva <danilo.silva@msn.com>
2021-10-19 16:17:46 -0300

    Adicionando Comentários Úteis

--- CSharpCollections/ListasDeObjetos/Program.cs
+++ CSharpCollections/ListasDeObjetos/Program.cs
@@ -7,21 +7,28 @@ namespace ListasDeObjetos
     {
         static void Main(string[] args)
         {
+            // Os elementos que entrarão na lista
             var aulaIntroducao = new Aula("Introdução às Coleções", 20);
             var aulaModelando = new Aula("Modelando a Classe Aula", 18);
             var aulaConjuntos = new Aula("Trabalhando com Conjuntos", 16);
 
+            // Declarando uma lista vazia
             List<Aula> aulas = new List<Aula>();
+            // Alimentando a lista com método Add
             aulas.Add(aulaIntroducao);
             aulas.Add(aulaModelando);
             aulas.Add(aulaConjuntos);
+
+            // Você não pode adicionar uma string se a lista é de Aula!
             //aulas.Add("Conclusão");
 
             Imprimir(aulas);
 
+            // Ordenando a lista pela ordem natural dos elementos (que não existe, a menos que Aula implemente IComparable)
             aulas.Sort();
             Imprimir(aulas);
 
+            // Ordenando a lista por uma ordem específica (por tempo) passando um Comparison como argumento
             aulas.Sort((esteObjeto, outroObjeto) => esteObjeto.Tempo.CompareTo(outroObjeto.Tempo));
             Imprimir(aulas);
         }
@@ -37,7 +44,7 @@ namespace ListasDeObjetos
         }
     }
 
-    class Aula : IComparable
+    class Aula : IComparable // O IComparable é necessário para usarmos o método Sort!
     {
         public Aula(string titulo, int tempo)
         {
@@ -48,7 +55,7 @@ namespace ListasDeObjetos
         public string Titulo { get; set; }
         public int Tempo { get; set; }
 
-        public int CompareTo(object obj)
+        public int CompareTo(object obj) // O CompareTo é necessário para usarmos o método Sort!
         {
             Aula aula = obj as Aula;
             return this.Titulo.CompareTo(aula.Titulo);
